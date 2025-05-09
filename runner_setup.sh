ef0f648b52ce42645da533014ede470d586e9fe5
m-shrief <m.shrief.dev@proton.me>
2025-05-09 11:27:57 +0300

    in the container, the file was not owned by any user, I've modified the bash script to make it accessible by root user, but as the container is rootless, I can't access it yet.

--- runner_setup.sh
+++ runner_setup.sh
@@ -2,13 +2,33 @@
 
 set -e
 
-mkdir -p data
-touch data/.runner
-mkdir -p data/.cache
+# Create the necessary directories and files
+sudo mkdir -p data
+sudo touch data/.runner
+sudo mkdir -p data/.cache
 
-chown -R 1001:1001 data/.runner
-chown -R 1001:1001 data/.cache
-chmod 775 data/.runner
-chmod 775 data/.cache
-chmod g+s data/.runner
-chmod g+s data/.cache
+# Change ownership to the user running the script
+sudo chown -R $(id -u):$(id -g) data/.runner
+sudo chown -R $(id -u):$(id -g) data/.cache
+
+# Set the appropriate permissions
+sudo chmod 775 data/.runner
+sudo chmod 775 data/.cache
+sudo chmod g+s data/.runner
+sudo chmod g+s data/.cache
+
+
+# #!/usr/bin/env bash
+
+# set -e
+
+# mkdir -p data
+# touch data/.runner
+# mkdir -p data/.cache
+
+# chown -R 1001:1001 data/.runner
+# chown -R 1001:1001 data/.cache
+# chmod 775 data/.runner
+# chmod 775 data/.cache
+# chmod g+s data/.runner
+# chmod g+s data/.cache
